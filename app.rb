3cc171056cb65e05f28fa5ca5019004ec878d313
Qudsia Ahmad and Rebecca McIntosh <qudsia+rebecca@>
2017-10-02 16:28:53 -0700

    pass the first integration spec.

--- app.rb
+++ app.rb
@@ -2,3 +2,31 @@ require("bundler/setup")
 Bundler.require(:default)
 
 Dir[File.dirname(__FILE__) + '/lib/*.rb'].each { |file| require file }
+
+get('/') do
+  @users = User.all()
+  @groups = Group.all
+  erb(:index)
+end
+
+post('/groups') do
+@id = params['id']
+@name = params['name']
+address = params['address']
+@email = params['email']
+@user = User.new({:name => @name, :address => address, :email => @email})
+@user.save
+erb(:groups)
+end
+
+
+
+post('/groups') do
+  @id = params['id']
+  @interest = params['interest']
+  @description = params['description']
+  binding.pry
+  @groups = Group.new({:interest => @interest, :description => @description, :id => 'id'})
+  @groups.save
+  erb(:groups)
+end
---
+++ spec/user_pages_spec.rb
@@ -0,0 +1,11 @@
+#The user should be able to enter their address to prove they have required address with a form requiring name and address.
+
+describe('viewing home and verify residency', {:type => :feature}) do
+  it('allows a user to input address') do
+    visit('/')
+
+    fill_in('address', :with => "1000 Vista Ave")
+    click_button('Submit')
+    expect(page).to have_content("Welcome")
+  end
+end
