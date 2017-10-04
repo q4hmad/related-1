c5b562cc2a0fae1a6cdbb67734b8359215e45485
Rebecca McIntosh <biffbangpow767@yahoo.com>
2017-10-03 21:12:32 -0700

    push users and services into groups and display them on the page.

--- app.rb
+++ app.rb
@@ -9,24 +9,62 @@ get('/') do
   erb(:index)
 end
 
-post('/groups') do
-@id = params['id']
-@name = params['name']
+post('/entry') do
+@groups = Group.all
+name = params['name']
 address = params['address']
-@email = params['email']
-@user = User.new({:name => @name, :address => address, :email => @email})
-@user.save
+email = params['email']
+@user = User.new({:name => name, :address => address, :email => email})
+if @user.save
 erb(:groups)
+else
+  (:deny)
+  end
+end
+
+get('/group/:id') do
+  @group = Group.find(params[:id])
+  @services = Services.all
+  @users = Users.all
+  erb(:services)
 end
 
 
-
-post('/groups') do
-  @id = params['id']
-  @interest = params['interest']
-  @description = params['description']
-  binding.pry
-  @groups = Group.new({:interest => @interest, :description => @description, :id => 'id'})
-  @groups.save
+post('/create_group') do
+  interest = params['group']
+  description = params['description']
+  @group = Group.new({:interest => interest, :description => description})
+  @group.save
+  @groups = Group.all
   erb(:groups)
+end
+
+get('/services/:id') do
+  id = params[:id]
+  @group = Group.find(params.fetch('id').to_i)
+  erb(:services)
+end
+
+post('/create_service/:id') do
+  @services = Service.all
+  @users = User.all
+  @group = Group.find(params.fetch('id').to_i)
+  location = params['service']
+  detail = params['detail']
+  @service = Service.new({:location => location, :detail => detail})
+  @service.save
+  @group.services.push(@service)
+  erb(:services)
+end
+
+post('/create_user/:id') do
+  @services = Service.all
+  @users = User.all
+  @group = Group.find(params.fetch('id').to_i)
+  name = params['name']
+  email = params['email']
+  @user = User.new({:name => name, :email => email})
+  @user.save
+  @group.users.push(@user)
+  erb(:services)
 end
